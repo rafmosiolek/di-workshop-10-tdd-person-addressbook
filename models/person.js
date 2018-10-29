c86eb98531f1d92f5539ba44dafc59665638f53a
RafM <rkjmosiolek@gmail.com>
2018-10-29 10:45:20 +0000

    create class Person, add fullName and addEmail methods with tests

--- models/person.js
+++ models/person.js
@@ -1,1 +1,18 @@
-// Your Person class goes here!
+class Person {
+    constructor(firstName, lastName, dateOfBirth) {
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.emails = [];
+    }
+
+    fullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    addEmail(email) {
+        this.emails.push(email);
+    }
+}
+
+module.exports = Person;
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,34 @@
 const chai = require('chai')
 const expect = chai.expect
 
-// Your Person test goes here!
+const Person = require('../models/person.js')
+
+describe('Person', () => {
+    it('should initialise properly', () => {
+        var person = new Person("Joe", "Biggs", "1 Jan 1990");
+        expect(person.firstName).to.equal("Joe");
+        expect(person.lastName).to.equal("Biggs");
+        expect(person.dateOfBirth).to.equal("1 Jan 1990");
+        expect(person.emails).to.deep.equal([]);
+    })
+    it('should capitalise first name', () => {
+        var person = new Person("joe", "Biggs", "1 Jan 1990");
+        expect(person.firstName).to.equal("Joe");
+    })
+
+    describe('Fullname method', () => {
+        it('should print the full name', () => {
+            var person = new Person("Alex", "Doe", "1 Jun 1989");
+            expect(person.fullName()).to.equal("Alex Doe");
+        })
+    })
+
+    describe('Add email method',() => {
+        it('should accept email and add it to emails array', () => {
+            var person = new Person("Alex", "Doe", "1 Jun 1989");
+            person.addEmail('alex.doe@gmail.com');
+            person.addEmail('alex.doe@example.com');
+            expect(person.emails).to.deep.equal(['alex.doe@gmail.com', 'alex.doe@example.com']);
+        })
+    })
+})
